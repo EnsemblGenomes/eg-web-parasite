aebe7b489594559aba9798309e7af21365c2eafc
Bruce Bolt <bbolt@ebi.ac.uk>
2014-04-08 12:00:06 +0100

    Amended footer copyright

---
+++ modules/EnsEMBL/Web/Document/Element/Copyright.pm
@@ -0,0 +1,40 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Document::Element::Copyright;
+
+### Copyright notice for footer (basic version with no logos)
+
+use strict;
+
+sub content {
+  my $self = shift;
+
+  my $sd = $self->species_defs;
+
+  return sprintf( qq(
+  <div class="twocol-left left">
+		   %s release %d - %s
+		  &copy; <span class="print_hide"><a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a> & <a href="http://www.sanger.ac.uk/" style="white-space:nowrap">WTSI</a></span>
+      <span class="screen_hide_inline">EBI & WTSI</span>
+  </div>),     $sd->SITE_NAME, $sd->SITE_RELEASE_VERSION, $sd->SITE_RELEASE_DATE
+	       );
+}
+
+1;
+
