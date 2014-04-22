136ea6c1ba49d5352868c109d35dfe75d507bd30
Bruce Bolt <bbolt@ebi.ac.uk>
2014-04-22 14:55:26 +0100

    Remove links to BioMart

---
+++ modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
@@ -0,0 +1,60 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Document::Element::ToolLinks;
+
+### Generates links to site tools - BLAST, help, login, etc (currently in masthead)
+
+use strict;
+
+sub content {
+  my $self    = shift;
+  my $hub     = $self->hub;
+  my $species = $hub->species;
+     $species = !$species || $species eq 'Multi' || $species eq 'common' ? 'Multi' : $species;
+  my @links; # = sprintf '<a class="constant" href="%s">Home</a>', $self->home;
+  push @links, qq{<a class="constant" href="/$species/blastview">BLAST</a>} if $self->blast;
+###EG  
+  if ($self->hub->species_defs->ENSEMBL_ENASEARCH_ENABLED) {
+      push @links,   '<a class="constant" href="/Multi/enasearch">Sequence Search</a>';
+  }
+
+  #push @links,   '<a class="constant" href="/biomart/martview">BioMart</a>';
+###
+  push @links,   '<a class="constant" href="/tools.html">Tools</a>';
+  push @links,   '<a class="constant" href="/downloads.html">Downloads</a>';
+### EG
+#  push @links,   '<a class="constant" href="/info/">Help &amp; Documentation</a>';
+  push @links,   '<a class="constant" href="/info/website/index.html">Help</a>';
+  push @links,   '<a class="constant" href="http://www.ensemblgenomes.org/info">Documentation</a>';
+### 
+  push @links,   '<a class="constant modal_link" href="/Help/Mirrors">Mirrors</a>' if keys %{$hub->species_defs->ENSEMBL_MIRRORS || {}};
+
+  my $last  = pop @links;
+  my $tools = join '', map "<li>$_</li>", @links;
+  
+  return qq{
+    <ul class="tools">$tools<li class="last">$last</li></ul>
+    <div class="more">
+      <a href="#">More <span class="arrow">&#9660;</span></a>
+    </div>
+  };
+}
+
+1;
+
