9470b65513cf706424c971e0cdcf125b600d95bc
Bruce Bolt <bbolt@ebi.ac.uk>
2014-05-19 16:46:28 +0100

    Changed capitalisation on species names

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -29,103 +29,29 @@ sub update_conf {
 
     map {delete($SiteDefs::__species_aliases{$_}) } keys %SiteDefs::__species_aliases;
 
-    $SiteDefs::ENSEMBL_PRIMARY_SPECIES  = 'Onchocerca_volvulus_PRJEB513'; ## Default species
-    $SiteDefs::ENSEMBL_SECONDARY_SPECIES  = 'Haemonchus_contortus_PRJEB506'; ## Default species
+    $SiteDefs::ENSEMBL_PRIMARY_SPECIES  = 'Onchocerca_volvulus_prjeb513'; ## Default species
+    $SiteDefs::ENSEMBL_SECONDARY_SPECIES  = 'Haemonchus_contortus_prjeb506'; ## Default species
     
 	# ParaSite Specific Species
 
-	# 50HGP species
-	#$SiteDefs::__species_aliases{'Acanthocheilonema_viteae_PRJEB4306'} = [qw(Acanthocheilonema_viteae_PRJEB4306)];
-	#$SiteDefs::__species_aliases{'Ancylostoma_caninum_PRJNA72585'} = [qw(Ancylostoma_caninum_PRJNA72585)];
-	#$SiteDefs::__species_aliases{'Ancylostoma_ceylanicum_PRJNA72583'} = [qw(Ancylostoma_ceylanicum_PRJNA72583)];
-	#$SiteDefs::__species_aliases{'Ancylostoma_duodenale_PRJNA72581'} = [qw(Ancylostoma_duodenale_PRJNA72581)];
-	#$SiteDefs::__species_aliases{'Angiostrongylus_cantonensis_PRJEB493'} = [qw(Angiostrongylus_cantonensis_PRJEB493)];
-	#$SiteDefs::__species_aliases{'Angiostrongylus_costaricensis_PRJEB494'} = [qw(Angiostrongylus_costaricensis_PRJEB494)];
-	#$SiteDefs::__species_aliases{'Anisakis_simplex_PRJEB496'} = [qw(Anisakis_simplex_PRJEB496)];
-	#$SiteDefs::__species_aliases{'Ascaris_lumbricoides_PRJEB495'} = [qw(Ascaris_lumbricoides_PRJEB495)];
-	$SiteDefs::__species_aliases{'Ascaris_suum_PRJNA62057'} = [qw(Ascaris_suum_PRJNA62057)];
-	$SiteDefs::__species_aliases{'Brugia_malayi_PRJNA10729'} = [qw(Brugia_malayi_PRJNA10729)];
-	#$SiteDefs::__species_aliases{'Brugia_pahangi_PRJEB497'} = [qw(Brugia_pahangi_PRJEB497)];
-	#$SiteDefs::__species_aliases{'Brugia_timori_PRJEB4663'} = [qw(Brugia_timori_PRJEB4663)];
-	$SiteDefs::__species_aliases{'Bursaphelenchus_xylophilus_PRJEA64437'} = [qw(Bursaphelenchus_xylophilus_PRJEA64437)];
-	#$SiteDefs::__species_aliases{'Clonorchis_sinensis_PRJDA72781'} = [qw(Clonorchis_sinensis_PRJDA72781)];
-	#$SiteDefs::__species_aliases{'Cylicostephanus_goldi_PRJEB498'} = [qw(Cylicostephanus_goldi_PRJEB498)];
-	#$SiteDefs::__species_aliases{'Dictyocaulus_viviparus_PRJNA72587'} = [qw(Dictyocaulus_viviparus_PRJNA72587)];
-	#$SiteDefs::__species_aliases{'Diphyllobothrium_latum_PRJEB1206'} = [qw(Diphyllobothrium_latum_PRJEB1206)];
-	$SiteDefs::__species_aliases{'Dirofilaria_immitis_PRJEB1797'} = [qw(Dirofilaria_immitis_PRJEB1797)];
-	#$SiteDefs::__species_aliases{'Dracunculus_medinensis_PRJEB500'} = [qw(Dracunculus_medinensis_PRJEB500)];
-	#$SiteDefs::__species_aliases{'Echinococcus_granulosus_PRJEB121'} = [qw(Echinococcus_granulosus_PRJEB121)];
-	#$SiteDefs::__species_aliases{'Echinococcus_multilocularis_PRJEB122'} = [qw(Echinococcus_multilocularis_PRJEB122)];
-	#$SiteDefs::__species_aliases{'Echinostoma_caproni_PRJEB1207'} = [qw(Echinostoma_caproni_PRJEB1207)];
-	#$SiteDefs::__species_aliases{'Elaeophora_elaphi_PRJEB502'} = [qw(Elaeophora_elaphi_PRJEB502)];
-	#$SiteDefs::__species_aliases{'Enterobius_vermicularis_PRJEB503'} = [qw(Enterobius_vermicularis_PRJEB503)];
-	#$SiteDefs::__species_aliases{'Fasciola_hepatica_PRJNA179522'} = [qw(Fasciola_hepatica_PRJNA179522)];
-	#$SiteDefs::__species_aliases{'Globodera_pallida_PRJEB123'} = [qw(Globodera_pallida_PRJEB123)];
-	#$SiteDefs::__species_aliases{'Gongylonema_pulchrum_PRJEB505'} = [qw(Gongylonema_pulchrum_PRJEB505)];
-	$SiteDefs::__species_aliases{'Haemonchus_contortus_PRJEB506'} = [qw(Haemonchus_contortus_PRJEB506)];
-	#$SiteDefs::__species_aliases{'Haemonchus_placei_PRJEB509'} = [qw(Haemonchus_placei_PRJEB509)];
-	#$SiteDefs::__species_aliases{'Heligmosomoides_bakeri_PRJEB1203'} = [qw(Heligmosomoides_bakeri_PRJEB1203)];
-	#$SiteDefs::__species_aliases{'Hydatigera_taeniaeformis_PRJEB534'} = [qw(Hydatigera_taeniaeformis_PRJEB534)];
-	#$SiteDefs::__species_aliases{'Hymenolepis_diminuta_PRJEB507'} = [qw(Hymenolepis_diminuta_PRJEB507)];
-	#$SiteDefs::__species_aliases{'Hymenolepis_microstoma_PRJEB124'} = [qw(Hymenolepis_microstoma_PRJEB124)];
-	#$SiteDefs::__species_aliases{'Hymenolepis_nana_PRJEB508'} = [qw(Hymenolepis_nana_PRJEB508)];
-	#$SiteDefs::__species_aliases{'Litomosoides_sigmodontis_PRJEB3075'} = [qw(Litomosoides_sigmodontis_PRJEB3075)];
-	$SiteDefs::__species_aliases{'Loa_loa_PRJNA60051'} = [qw(Loa_loa_PRJNA60051)];
-	$SiteDefs::__species_aliases{'Meloidogyne_hapla_PRJNA29083'} = [qw(Meloidogyne_hapla_PRJNA29083)];
-	#$SiteDefs::__species_aliases{'Mesocestoides_corti_PRJEB510'} = [qw(Mesocestoides_corti_PRJEB510)];
-	$SiteDefs::__species_aliases{'Necator_americanus_PRJNA72135'} = [qw(Necator_americanus_PRJNA72135)];
-	#$SiteDefs::__species_aliases{'Nippostrongylus_brasiliensis_PRJEB511'} = [qw(Nippostrongylus_brasiliensis_PRJEB511)];
-	#$SiteDefs::__species_aliases{'Oesophagostomum_dentatum_PRJNA72579'} = [qw(Oesophagostomum_dentatum_PRJNA72579)];
-	#$SiteDefs::__species_aliases{'Onchocerca_flexuosa_PRJEB512'} = [qw(Onchocerca_flexuosa_PRJEB512)];
-	#$SiteDefs::__species_aliases{'Onchocerca_ochengi_PRJEB1809'} = [qw(Onchocerca_ochengi_PRJEB1809)];
-	$SiteDefs::__species_aliases{'Onchocerca_volvulus_PRJEB513'} = [qw(Onchocerca_volvulus_PRJEB513)];
-	#$SiteDefs::__species_aliases{'Parascaris_equorum_PRJEB514'} = [qw(Parascaris_equorum_PRJEB514)];
-	#$SiteDefs::__species_aliases{'Parastrongyloides_trichosuri_PRJEB515'} = [qw(Parastrongyloides_trichosuri_PRJEB515)];
-	#$SiteDefs::__species_aliases{'Pristionchus_pacificus_PRJNA12644'} = [qw(Pristionchus_pacificus_PRJNA12644)];
-	#$SiteDefs::__species_aliases{'Protopolystoma_xenopodis_PRJEB1201'} = [qw(Protopolystoma_xenopodis_PRJEB1201)];
-	#$SiteDefs::__species_aliases{'Rhabditophanes_sp_PRJEB1297'} = [qw(Rhabditophane_sp_PRJEB1297)];
-	#$SiteDefs::__species_aliases{'Romanomermis_culicivorax_PRJEB1358'} = [qw(Romanomermis_culicivorax_PRJEB1358)];
-	#$SiteDefs::__species_aliases{'Schistocephalus_solidus_PRJEB527'} = [qw(Schistocephalus_solidus_PRJEB527)];
-	#$SiteDefs::__species_aliases{'Schistosoma_curassoni_PRJEB519'} = [qw(Schistosoma_curassoni_PRJEB519)];
-	#$SiteDefs::__species_aliases{'Schistosoma_haematobium_PRJNA78265'} = [qw(Schistosoma_haematobium_PRJNA78265)];
-	#$SiteDefs::__species_aliases{'Schistosoma_japonicum_PRJEA34885'} = [qw(Schistosoma_japonicum_PRJEA34885)];
-	#$SiteDefs::__species_aliases{'Schistosoma_mansoni_PRJEA36577'} = [qw(Schistosoma_mansoni_PRJEA36577)];
-	#$SiteDefs::__species_aliases{'Schistosoma_margrebowiei_PRJEB522'} = [qw(Schistosoma_margrebowiei_PRJEB522)];
-	#$SiteDefs::__species_aliases{'Schistosoma_mattheei_PRJEB523'} = [qw(Schistosoma_mattheei_PRJEB523)];
-	#$SiteDefs::__species_aliases{'Schistosoma_rodhaini_PRJEB526'} = [qw(Schistosoma_rodhaini_PRJEB526)];
-	#$SiteDefs::__species_aliases{'Schmidtea_mediterranea_PRJNA12585'} = [qw(Schmidtea_mediterranea_PRJNA12585)];
-	#$SiteDefs::__species_aliases{'Soboliphyme_baturini_PRJEB516'} = [qw(Soboliphyme_baturini_PRJEB516)];
-	#$SiteDefs::__species_aliases{'Spirometra_erinaceieuropaei_PRJEB1202'} = [qw(Spirometra_erinaceieuropaei_PRJEB1202)];
-	#$SiteDefs::__species_aliases{'Strongyloides_papillosus_PRJEB525'} = [qw(Strongyloides_papillosus_PRJEB525)];
-	#$SiteDefs::__species_aliases{'Strongyloides_ratti_PRJEB125'} = [qw(Strongyloides_ratti_PRJEB125)];
-	#$SiteDefs::__species_aliases{'Strongyloides_stercoralis_PRJEB528'} = [qw(Strongyloides_stercoralis_PRJEB528)];
-	#$SiteDefs::__species_aliases{'Strongyloides_venezuelensis_PRJEB530'} = [qw(Strongyloides_venezuelensis_PRJEB530)];
-	#$SiteDefs::__species_aliases{'Strongylus_vulgaris_PRJEB531'} = [qw(Strongylus_vulgaris_PRJEB531)];
-	#$SiteDefs::__species_aliases{'Syphacia_muris_PRJEB524'} = [qw(Syphacia_muris_PRJEB524)];
-	#$SiteDefs::__species_aliases{'Taenia_asiatica_PRJEB532'} = [qw(Taenia_asiatica_PRJEB532)];
-	#$SiteDefs::__species_aliases{'Taenia_solium_PRJNA170813'} = [qw(Taenia_solium_PRJNA170813)];
-	#$SiteDefs::__species_aliases{'Teladorsagia_circumcincta_PRJNA72569'} = [qw(Teladorsagia_circumcincta_PRJNA72569)];
-	#$SiteDefs::__species_aliases{'Thelazia_callipaeda_PRJEB1205'} = [qw(Thelazia_callipaeda_PRJEB1205)];
-	#$SiteDefs::__species_aliases{'Toxocara_canis_PRJEB533'} = [qw(Toxocara_canis_PRJEB533)];
-	#$SiteDefs::__species_aliases{'Trichinella_nativa_PRJNA179527'} = [qw(Trichinella_nativa_PRJNA179527)];
-	#$SiteDefs::__species_aliases{'Trichobilharzia_regenti_PRJEB4662'} = [qw(Trichobilharzia_regenti_PRJEB4662)];
-	$SiteDefs::__species_aliases{'Trichinella_spiralis_PRJNA12603'} = [qw(Trichinella_spiralis_PRJNA12603)];
-	#$SiteDefs::__species_aliases{'Trichuris_muris_PRJEB126'} = [qw(Trichuris_muris_PRJEB126)];
-	#$SiteDefs::__species_aliases{'Trichuris_suis_PRJNA179528'} = [qw(Trichuris_suis_PRJNA179528)];
-	#$SiteDefs::__species_aliases{'Trichuris_trichiura_PRJEB535'} = [qw(Trichuris_trichiura_PRJEB535)];
-	#$SiteDefs::__species_aliases{'Wuchereria_bancrofti_PRJEB536'} = [qw(Wuchereria_bancrofti_PRJEB536)];
-	#
-	## ParaSite specific species for release 1
-	$SiteDefs::__species_aliases{'Ancylostoma_ceylanicum_PRJNA231479'} = [qw(Ancylostoma_ceylanicum_PRJNA231479)];
-	$SiteDefs::__species_aliases{'Ascaris_suum_PRJNA80881'} = [qw(Ascaris_suum_PRJNA80881)];
-	#$SiteDefs::__species_aliases{'Dictyocaulus_viviparus_PRJEB5116'} = [qw(Dictyocaulus_viviparus_PRJEB5116)];
-	$SiteDefs::__species_aliases{'Haemonchus_contortus_PRJNA205202'} = [qw(Haemonchus_contortus_PRJNA205202)];
-	$SiteDefs::__species_aliases{'Heterorhabditis_bacteriophora_PRJNA13977'} = [qw(Heterorhabditis_bacteriophora_PRJNA13977)];
-	#$SiteDefs::__species_aliases{'Meloidogyne_floridensis_PRJEB2953'} = [qw(Meloidogyne_floridensis_PRJEB2953)];
-	$SiteDefs::__species_aliases{'Meloidogyne_incognita_PRJEA28837'} = [qw(Meloidogyne_incognita_PRJEA28837)];
-	#$SiteDefs::__soecies_aliases{'Onchocerca_ochengi_PRJEB1204'} = [qw(Onchocerca_ochengi_PRJEB1204)];
-        $SiteDefs::__species_aliases{'Trichuris_suis_PRJNA208415'} = [qw(Trichuris_suis_PRJNA208415)];
-	$SiteDefs::__species_aliases{'Trichuris_suis_PRJNA208416'} = [qw(Trichuris_suis_PRJNA208416)];
+	# Release 0.1
+	$SiteDefs::__species_aliases{'Ancylostoma_ceylanicum_prjna231479'} = [qw(ancylostoma_ceylanicum_prjna231479, Ancylostoma_ceylanicum_prjna231479)];
+	$SiteDefs::__species_aliases{'Ascaris_suum_prjna62057'} = [qw(ascaris_suum_prjna62057, Ascaris_suum_prjna62057)];
+	$SiteDefs::__species_aliases{'Ascaris_suum_prjna80881'} = [qw(ascaris_suum_prjna80881, Ascaris_suum_prjna80881)];
+	$SiteDefs::__species_aliases{'Brugia_malayi_prjna10729'} = [qw(brugia_malayi_prjna10729, Brugia_malayi_prjna10729)];
+	$SiteDefs::__species_aliases{'Bursaphelenchus_xylophilus_prjea64437'} = [qw(bursaphelenchus_xylophilus_prjea64437, Bursaphelenchus_xylophilus_prjea64437)];
+	$SiteDefs::__species_aliases{'Dirofilaria_immitis_prjeb1797'} = [qw(dirofilaria_immitis_prjeb1797, Dirofilaria_immitis_prjeb1797)];
+	$SiteDefs::__species_aliases{'Haemonchus_contortus_prjeb506'} = [qw(haemonchus_contortus_prjeb506, Haemonchus_contortus_prjeb506)];
+	$SiteDefs::__species_aliases{'Haemonchus_contortus_prjna205202'} = [qw(haemonchus_contortus_prjna205202, Haemonchus_contortus_prjna205202)];
+	$SiteDefs::__species_aliases{'Heterorhabditis_bacteriophora_prjna13977'} = [qw(heterorhabditis_bacteriophora_prjna13977, Heterorhabditis_bacteriophora_prjna13977)];
+	$SiteDefs::__species_aliases{'Loa_loa_prjna60051'} = [qw(loa_loa_prjna60051, Loa_loa_prjna60051)];
+	$SiteDefs::__species_aliases{'Meloidogyne_hapla_prjna29083'} = [qw(meloidogyne_hapla_prjna29083, Meloidogyne_hapla_prjna29083)];
+	$SiteDefs::__species_aliases{'Meloidogyne_incognita_prjea28837'} = [qw(meloidogyne_incognita_prjea28837, Meloidogyne_incognita_prjea28837)];
+	$SiteDefs::__species_aliases{'Necator_americanus_prjna72135'} = [qw(necator_americanus_prjna72135, Necator_americanus_prjna72135)];
+	$SiteDefs::__species_aliases{'Onchocerca_volvulus_prjeb513'} = [qw(onchocerca_volvulus_prjeb513, Onchocerca_volvulus_prjeb513)];
+	$SiteDefs::__species_aliases{'Trichinella_spiralis_prjna12603'} = [qw(trichinella_spiralis_prjna12603, Trichinella_spiralis_prjna12603)];
+	$SiteDefs::__species_aliases{'Trichuris_suis_prjna208415'} = [qw(trichuris_suis_prjna208415, Trichuris_suis_prjna208415)];
+	$SiteDefs::__species_aliases{'Trichuris_suis_prjna208416'} = [qw(trichuris_suis_prjna208416, Trichuris_suis_prjna208416)];
 
 	@SiteDefs::ENSEMBL_PERL_DIRS    = (
 										   $SiteDefs::ENSEMBL_WEBROOT.'/perl',
