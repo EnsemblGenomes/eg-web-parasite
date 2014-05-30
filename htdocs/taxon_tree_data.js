4437af3171a6b13d5c25edc7ea8266752db931be
Bruce Bolt <bbolt@ebi.ac.uk>
2014-05-30 14:09:51 +0100

    Added taxon tree

---
+++ htdocs/taxon_tree_data.js
@@ -0,0 +1,253 @@
+taxonTreeData = [
+   {
+      "isFolder" : true,
+      "children" : [
+         {
+            "isFolder" : true,
+            "children" : [
+               {
+                  "isFolder" : true,
+                  "children" : [
+                     {
+                        "title" : "Ascaris suum",
+                        "key" : "ascaris_suum_prjna62057"
+                     },
+                     {
+                        "title" : "Ascaris suum",
+                        "key" : "ascaris_suum_prjna80881"
+                     }
+                  ],
+                  "title" : "Ascaridida (1)",
+                  "key" : "Ascaridida"
+               },
+               {
+                  "isFolder" : true,
+                  "children" : [
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "title" : "Heterorhabditis bacteriophora",
+                              "key" : "heterorhabditis_bacteriophora_prjna13977"
+                           }
+                        ],
+                        "title" : "Rhabditoidea (1)",
+                        "key" : "Rhabditoidea"
+                     },
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "isFolder" : true,
+                                    "children" : [
+                                       {
+                                          "isFolder" : true,
+                                          "children" : [
+                                             {
+                                                "title" : "Ancylostoma ceylanicum",
+                                                "key" : "ancylostoma_ceylanicum_prjna231479"
+                                             }
+                                          ],
+                                          "title" : "Ancylostomatinae (1)",
+                                          "key" : "Ancylostomatinae"
+                                       },
+                                       {
+                                          "isFolder" : true,
+                                          "children" : [
+                                             {
+                                                "title" : "Necator americanus",
+                                                "key" : "necator_americanus_prjna72135"
+                                             }
+                                          ],
+                                          "title" : "Bunostominae (1)",
+                                          "key" : "Bunostominae"
+                                       }
+                                    ],
+                                    "title" : "Ancylostomatidae (2)",
+                                    "key" : "Ancylostomatidae"
+                                 }
+                              ],
+                              "title" : "Ancylostomatoidea (2)",
+                              "key" : "Ancylostomatoidea"
+                           },
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Haemonchus contortus",
+                                    "key" : "haemonchus_contortus_prjeb506"
+                                 },
+                                 {
+                                    "title" : "Haemonchus contortus",
+                                    "key" : "haemonchus_contortus_prjna205202"
+                                 }
+                              ],
+                              "title" : "Trichostrongyloidea (1)",
+                              "key" : "Trichostrongyloidea"
+                           }
+                        ],
+                        "title" : "Strongylida (3)",
+                        "key" : "Strongylida"
+                     }
+                  ],
+                  "title" : "Rhabditida (4)",
+                  "key" : "Rhabditida"
+               },
+               {
+                  "isFolder" : true,
+                  "children" : [
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Brugia malayi",
+                                    "key" : "brugia_malayi_prjna10729"
+                                 }
+                              ],
+                              "title" : "Brugia (1)",
+                              "key" : "Brugia"
+                           },
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Dirofilaria immitis",
+                                    "key" : "dirofilaria_immitis_prjeb1797"
+                                 }
+                              ],
+                              "title" : "Dirofilaria (1)",
+                              "key" : "Dirofilaria"
+                           },
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Loa loa",
+                                    "key" : "loa_loa_prjna60051"
+                                 }
+                              ],
+                              "title" : "Loa (1)",
+                              "key" : "Loa"
+                           },
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Onchocerca volvulus",
+                                    "key" : "onchocerca_volvulus_prjeb513"
+                                 }
+                              ],
+                              "title" : "Onchocerca (1)",
+                              "key" : "Onchocerca"
+                           }
+                        ],
+                        "title" : "Onchocercidae (4)",
+                        "key" : "Onchocercidae"
+                     }
+                  ],
+                  "title" : "Spirurida (4)",
+                  "key" : "Spirurida"
+               },
+               {
+                  "isFolder" : true,
+                  "children" : [
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "title" : "Bursaphelenchus xylophilus",
+                              "key" : "bursaphelenchus_xylophilus_prjea64437"
+                           }
+                        ],
+                        "title" : "Aphelenchina (1)",
+                        "key" : "Aphelenchina"
+                     },
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "isFolder" : true,
+                              "children" : [
+                                 {
+                                    "title" : "Meloidogyne hapla",
+                                    "key" : "meloidogyne_hapla_prjna29083"
+                                 },
+                                 {
+                                    "isFolder" : true,
+                                    "children" : [
+                                       {
+                                          "title" : "Meloidogyne incognita",
+                                          "key" : "meloidogyne_incognita_prjea28837"
+                                       }
+                                    ],
+                                    "title" : "Meloidogyne incognita group (1)",
+                                    "key" : "Meloidogyne incognita group"
+                                 }
+                              ],
+                              "title" : "Meloidogyne (2)",
+                              "key" : "Meloidogyne"
+                           }
+                        ],
+                        "title" : "Tylenchina (2)",
+                        "key" : "Tylenchina"
+                     }
+                  ],
+                  "title" : "Tylenchida (3)",
+                  "key" : "Tylenchida"
+               }
+            ],
+            "title" : "Chromadorea (12)",
+            "key" : "Chromadorea"
+         },
+         {
+            "isFolder" : true,
+            "children" : [
+               {
+                  "isFolder" : true,
+                  "children" : [
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "title" : "Trichinella spiralis",
+                              "key" : "trichinella_spiralis_prjna12603"
+                           }
+                        ],
+                        "title" : "Trichinellidae (1)",
+                        "key" : "Trichinellidae"
+                     },
+                     {
+                        "isFolder" : true,
+                        "children" : [
+                           {
+                              "title" : "Trichuris suis",
+                              "key" : "trichuris_suis_prjna208416"
+                           },
+                           {
+                              "title" : "Trichuris suis",
+                              "key" : "trichuris_suis_prjna208415"
+                           }
+                        ],
+                        "title" : "Trichuridae (1)",
+                        "key" : "Trichuridae"
+                     }
+                  ],
+                  "title" : "Trichocephalida (2)",
+                  "key" : "Trichocephalida"
+               }
+            ],
+            "title" : "Enoplea (2)",
+            "key" : "Enoplea"
+         }
+      ],
+      "title" : "Nematoda (14)",
+      "key" : "Nematoda"
+   }
+]
+;
