8212a657e654821266ff1591e0de36a02d45d248
Bruce Bolt <bbolt@ebi.ac.uk>
2014-07-02 12:20:11 +0100

    Adjust width of search box on species homepage

--- modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
+++ modules/EnsEMBL/Web/Document/HTML/HomeSearch.pm
@@ -39,7 +39,7 @@ sub render {
   my $search_url          = $species_defs->ENSEMBL_WEB_ROOT . "$page_species/psychic";
   my $default_search_code = $species_defs->ENSEMBL_DEFAULT_SEARCHCODE;
   my $is_home_page        = $page_species eq 'Multi';
-  my $input_size          = $is_home_page ? 30 : 50;
+  my $input_size          = $is_home_page ? 30 : 40;
   my $favourites          = $hub->get_favourite_species;
   my $q                   = $hub->param('q');
 
