5310ca8229965243fbc4a44ade8b2f7ac79f8755
Bruce Bolt <bbolt@ebi.ac.uk>
2014-08-18 14:10:24 +0100

    Moved about to bottom of homepage, expanded search boxes

--- modules/EnsEMBL/Web/Document/HTML/GenomeCollectionsList.pm
+++ modules/EnsEMBL/Web/Document/HTML/GenomeCollectionsList.pm
@@ -27,7 +27,6 @@ sub render {
   return qq{
       <div id="species_list" class="js_panel home-search-flex">
         <input type="hidden" class="panel_type" value="SpeciesList" />
-        <h3 class="first">Search for a genome</h3>
         <form id="species_autocomplete_form" action="/info/about/species.html" style="margin-bottom:5px" method="get">
           <div>
            <input name="search" type="text" id="species_autocomplete" class="ui-autocomplete-input inactive" style="width:95\%; margin: 0" placeholder="Start typing the name of a genome...">
