566dfbbc923e137fa1038837f6dc56254ed37f74
Bruce Bolt <bbolt@ebi.ac.uk>
2014-08-27 16:37:42 +0100

    Updated format of heldesk email

--- modules/EnsEMBL/Web/Mailer/Help.pm
+++ modules/EnsEMBL/Web/Mailer/Help.pm
@@ -33,31 +33,12 @@ sub report_header {
   return join "\n", map { sprintf '%-16.16s: %s', $_->[0], $_->[1] } (
     ['Date'       => sprintf('%04d-%02d-%02d %02d:%02d:%02d', $T[5]+1900, $T[4]+1, $T[3], $T[2], $T[1], $T[0])],
     ['Name'       => $hub->param('name')                                                                      ],
+    ['Email'      => $hub->param('address')                                                                   ],
     ['Referer'    => $hub->species_defs->ENSEMBL_SERVERNAME                                                   ],
+    ['IP'         => $ENV{'REMOTE_ADDR'}                                                                      ],
     ['User agent' => $ENV{'HTTP_USER_AGENT'}                                                                  ],
     @_
   );
-}
-
-sub send_movie_feedback_email {
-  my $self        = shift;
-  my $problems    = shift;
-  my $hub         = $self->hub;
-  my $sd          = $hub->species_defs;
-  my %problems    = map { $_->{'value'} => $_->{'caption'} } @{$problems};
-
-  $self->to       = $sd->ENSEMBL_HELPDESK_EMAIL;
-  $self->from     = $hub->param('email');
-  $self->subject  = ($hub->param('subject') || $sd->ENSEMBL_SITETYPE . ' Helpdesk') . ' - ' . $sd->ENSEMBL_SERVERNAME;
-  $self->message  = sprintf "Feedback from %s\n\n%s\n\nProblem with video\n\n%s\n\n%s\n\nComments:\n\n%s",
-    $sd->ENSEMBL_SERVERNAME,
-    $self->report_header,
-    join('', map { "* $problems{$_}\n" } $hub->param('problem')),
-    $hub->param('title'),
-    $hub->param('text')
-  ;
-
-  return $self->send;
 }
 
 sub send_help_contact_email {
@@ -67,7 +48,7 @@ sub send_help_contact_email {
 
   $self->to       = $sd->ENSEMBL_HELPDESK_EMAIL;
   $self->from     = $hub->param('address');
-  $self->subject  = ($hub->param('subject') || $sd->ENSEMBL_SITETYPE . ' Helpdesk') . ' - ' . $sd->ENSEMBL_SERVERNAME;
+  $self->subject  = 'USER EMAIL: ' . ($hub->param('subject') || '(no subject)');
   $self->message  = sprintf "Support question from %s\n\n%s\n\nComments:\n\n%s",
     $sd->ENSEMBL_SERVERNAME,
     $self->report_header([ 'Last Search', $hub->param('string')||'-none-' ]),
