464d86b31c81e608971bd68ab06505c6d7b76ffb
Bruce Bolt <bbolt@ebi.ac.uk>
2014-09-11 15:39:37 +0100

    Updated error on external references page

--- modules/EnsEMBL/Web/Component/Gene/SimilarityMatches.pm
+++ modules/EnsEMBL/Web/Component/Gene/SimilarityMatches.pm
@@ -32,7 +32,7 @@ sub content {
   my $self       = shift;
   my @dbtypes   = qw(MISC LIT);
   my $matches    = $self->_matches('similarity_matches', 'Similarity Matches', 'PRIMARY_DB_SYNONYM', @dbtypes, 'RenderAsTables');
-  my $no_matches = qq(<p>No external references assigned to this gene.</p><br />);
+  my $no_matches = qq();
   my $html       = $matches ? $matches : $no_matches;
   $html         .= $self->matches_to_html(@dbtypes);
   return $html;
@@ -96,109 +96,13 @@ sub matches_to_html {
   $table->add_rows(@rows);
 
   if ($count_ext_refs == 0) {
-    $html.= '<p><strong>No (selected) external database contains identifiers which correspond to the transcripts of this gene.</strong></p>';
+    $html.= '<p>No external database contains identifiers which correspond to the transcripts of this gene.</p>';
   } else {
     $html .= '<p><strong>The following database identifier' . ($count_ext_refs > 1 ? 's' : '') . ' correspond' . ($count_ext_refs > 1 ? '' : 's') . ' to the transcripts of this gene:</strong></p>';
     $html .= $table->render;
   }
 
   return $html;
-}
-
-sub format_column_header {
-  my $self            = shift;
-  my $column_header   = shift;
-  $column_header      =~ s/\//\/ /; # add a space after a /, which enables the table header to split the name into multiple lines if needed.
-  return $column_header;
-}
-
-sub get_matches_by_transcript {
-  my $self          = shift;
-  my $transcript    = shift;
-  my @dbtypes         = @_;
-  my %allowed_types = map { $_ => 1 } @dbtypes;
-  my $db_links;
-  my @return_links;
-
-  eval {
-    $db_links = $transcript->get_all_DBLinks;
-  };
-
-  foreach (@$db_links) {
-    if (defined $allowed_types{$_->type}) {
-      $_->{'transcript'} = $transcript;
-      push @return_links, $_;
-    }
-  }
-  return @return_links;
-}
-
-sub get_similarity_links_hash {
-   my $self       = shift;
-   my $type       = shift;
-   my $hub        = $self->hub;
-   my $urls       = $hub->ExtURL;
-   my $fv_type    = $hub->action eq 'Oligos' ? 'OligoFeature' : 'Xref'; # default link to featureview is to retrieve an Xref
-   my $externalDB = $type->database;
-   my $display_id = $type->display_id;
-
-   #one day should sort out the database so that we don't have to do these
-   next if $type->status eq 'ORTH';                            # remove all orthologs
-   next if lc $externalDB eq 'medline';                        # ditch medline entries - redundant as we also have pubmed
-   next if $externalDB =~ /^flybase/i && $display_id =~ /^CG/; # ditch celera genes from FlyBase
-   next if $externalDB eq 'Vega_gene';                         # remove internal links to self and transcripts
-   next if $externalDB eq 'Vega_transcript' && $display_id !~ /OTT/; #only show OTT xrefs
-   next if $externalDB eq 'Vega_translation' && $display_id !~ /OTT/; #only show OTT xrefs;
-   next if $externalDB eq 'OTTP' && $display_id =~ /^\d+$/;    # don't show vega translation internal IDs   
-
-   my $text       = $display_id;
-   my $primary_id = $type->primary_id;
-   my %similarity_links;
-
-   # hack for LRG in e58
-   $primary_id =~ s/_g\d*$// if $externalDB eq 'ENS_LRG_gene';
-
-   (my $A = $externalDB) =~ s/_predicted//;
-
-   $type->{ID} = $primary_id;
-   $similarity_links{'link'} = $urls->get_url($A, $type) if $urls && $urls->is_linked($A);
-
-   $similarity_links{'link_text'} = $A eq 'MARKERSYMBOL' ? " ($primary_id)" : $display_id;
-
-   if ($hub->species_defs->ENSEMBL_PFETCH_SERVER && $externalDB =~ /^(SWISS|SPTREMBL|LocusLink|protein_id|RefSeq|EMBL|Gene-name|Uniprot)/i) {
-     my $seq_arg = $display_id;
-     $seq_arg    = "LL_$seq_arg" if $externalDB eq 'LocusLink';
-     my $url = $hub->url({
-       type     => 'Transcript',
-       action   => 'Similarity/Align',
-       sequence => $seq_arg,
-       extdb    => lc $externalDB,
-     });
-     $similarity_links{'align_url'} = $url;
-   }
-   $similarity_links{'search_go_link'} = $urls->get_url('GOSEARCH', $primary_id) if $externalDB =~ /^(SWISS|SPTREMBL)/i; # add Search GO link;
-
-    # add link to featureview
-    ## FIXME - another LRG hack! 
-    my $all_locations_url;
-    if ($externalDB eq 'ENS_LRG_gene') {
-      $all_locations_url = $hub->url({
-        type    => 'LRG',
-        action  => 'Genome',
-        lrg     => $display_id,
-      });
-    } else {
-      my $link_name = $fv_type eq 'OligoFeature' ? $display_id : $primary_id;
-      my $link_type = $fv_type eq 'OligoFeature' ? $fv_type : "${fv_type}_$externalDB";
-      $all_locations_url = $hub->url({
-        type   => 'Location',
-        action => 'Genome',
-        id     => $link_name,
-        ftype  => $link_type
-      });
-    }
-    $similarity_links{'all_locations_url'} = $all_locations_url;
-    return %similarity_links;
 }
 
 1;
