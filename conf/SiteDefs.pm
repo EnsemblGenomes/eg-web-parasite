7714ac8d5a150ef23d53d78e5ec0ecba0be14ecf
Eugene Kulesha <ek@ebi.ac.uk>
2014-10-08 12:18:28 +0100

    use parasite specific dbs for session and blast

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -23,7 +23,7 @@ sub update_conf {
 
     $SiteDefs::SITE_RELEASE_VERSION = '1';
     $SiteDefs::SITE_RELEASE_DATE = 'September 2014';
-
+    $SiteDefs::ENSEMBL_USERDB_NAME = 'ensembl_accounts_pt';
     $SiteDefs::GENOMIC_UNIT = 'parasite';
     $SiteDefs::EBEYE_SEARCH_UNITS = [qw(parasite)];
     $SiteDefs::EBEYE_SITE_NAMES = {
