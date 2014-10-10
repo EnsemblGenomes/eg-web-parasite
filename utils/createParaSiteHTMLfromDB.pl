8b5c44506e7b3583f8056141a24b44c184e37440
Bruce Bolt <bbolt@ebi.ac.uk>
2014-10-10 11:03:21 +0100

    Added script to generate static content from production database

---
+++ utils/createParaSiteHTMLfromDB.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use DBI;
+
+my $outdir = "/nfs/public/rw/ensembl/websites/parasite/current/eg-web-parasite/htdocs/ssi/species";
+
+my @files_to_delete = <$outdir/about_*_*.html>;
+unlink(@files_to_delete);
+
+##############
+
+my $dbh = DBI->connect("DBI:mysql:ensembl_production_parasite;host=mysql-eg-pan-1:4276", 'ensro')
+    || die "Could not connect to database: $DBI::errstr";
+
+my $sql = "SELECT species_name, summary, assembly, annotation, resources, publication FROM static_genome";
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+
+my %keys = (1 => 'summary', 2 => 'assembly', 3 => 'annotation', 4 => 'resources', 5 => 'other');
+my %labels = (1 => 'Summary', 2 => 'Assembly', 3 => 'Annotation', 4 => 'Resources', 5 => 'Key Publications');
+
+while (my $result = $sth->fetchrow_arrayref) {
+	my @results = @{$result};
+	print "$results[0]\n";
+	open(OUTFILE, ">$outdir/about_$results[0].html");
+	for(my $i = 1; $i <= 5; $i++) {
+		if($results[$i] ne '') {
+			print OUTFILE qq(<!-- \{$keys{$i}\} --><a name="$keys{$i}"></a>\n);
+			print OUTFILE qq(<h2>$labels{$i}</h2>\n);
+			print OUTFILE qq(<p>$results[$i]</p>\n);
+			print OUTFILE qq(<!-- \{$keys{$i}\} -->\n\n);
+		}
+	}
+	close(OUTFILE);
+}
+
+##############
+
+my $sql = "SELECT species_name, display_name, description FROM static_species";
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+
+my %keys = (1 => 'about');
+my %labels = (1 => 'About %s');
+
+while (my $result = $sth->fetchrow_arrayref) {
+	my @results = @{$result};
+	print "$results[0]\n";
+	open(OUTFILE, ">/$outdir/about_$results[0].html");
+	if($results[0] ne '') {
+		my $header = sprintf($labels{1}, "<em>$results[1]</em>");
+		print OUTFILE qq(<!-- \{$keys{1}\} --><a name="$keys{1}"></a>\n);
+		print OUTFILE qq(<h2>$header</h2>\n);
+		print OUTFILE qq(<p>$results[2]</p>\n);
+		print OUTFILE qq(<!-- \{$keys{1}\} -->\n\n);
+	}
+	close(OUTFILE);
+}
