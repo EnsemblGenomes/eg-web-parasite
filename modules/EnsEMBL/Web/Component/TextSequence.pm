8f1e15f4ca71c3f4034b277f24d54ff6c0a7581d
Bruce Bolt <bbolt@ebi.ac.uk>
2014-11-12 16:32:19 +0000

    Removed BlastView

--- modules/EnsEMBL/Web/Component/TextSequence.pm
+++ modules/EnsEMBL/Web/Component/TextSequence.pm
@@ -21,38 +21,16 @@ limitations under the License.
 package EnsEMBL::Web::Component::TextSequence;
 
 use strict;
-  
 
-sub tool_buttons {
-  my ($self, $blast_seq, $peptide) = @_;
-  
-  return unless $self->html_format;
-  
-  my $hub  = $self->hub;
-  my $html = sprintf('
-    <div class="other_tool">
-      <p><a class="seq_export export" href="%s">Download view as RTF</a></p>
-    </div>', 
-    $self->ajax_url('rtf', { filename => join('_', $hub->type, $hub->action, $hub->species, $self->object->Obj->stable_id), _format => 'RTF' })
-  );
-  
-  if ($blast_seq && $hub->species_defs->ENSEMBL_BLAST_ENABLED) {
-    $html .= sprintf('
-      <div class="other_tool">
-        <p><a class="seq_blast find" href="#">BLAST this sequence</a></p>
-        <form class="external hidden seq_blast" action="/Multi/blastview" method="post">
-          <fieldset>
-            <input type="hidden" name="_query_sequence" value="%s" />
-            <input type="hidden" name="species" value="%s" />
-            %s
-          </fieldset>
-        </form>
-      </div>',
-      $blast_seq, $hub->species, $peptide ? '<input type="hidden" name="query" value="peptide" /><input type="hidden" name="database" value="peptide" />' : ''
-    );
-  }
-
-  return $html;
+use previous qw(buttons);
+ 
+sub buttons {
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $species_defs = $hub->species_defs;
+  my @buttons      = $self->PREV::buttons(@_);
+  pop @buttons; # Remove the enasearch button
+  return @buttons;
 }
 
 1;
