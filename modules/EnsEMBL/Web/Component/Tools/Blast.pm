d09c8be7723b203659304a18c669428857c695b1
Bruce Bolt <bbolt@ebi.ac.uk>
2015-01-06 11:00:01 +0000

    Remove thumbnail image from BLAST results [PARASITE-68]

---
+++ modules/EnsEMBL/Web/Component/Tools/Blast.pm
@@ -0,0 +1,61 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Component::Tools::Blast;
+
+sub job_details_table {
+  ## A two column layout displaying a job's details
+  ## @param Job object
+  ## @params Extra params as required by get_job_summary method
+  ## @return DIV node (as returned by new_twocol method)
+  my ($self, $job) = splice @_, 0, 2;
+
+  my $object      = $self->object;
+  my $hub         = $self->hub;
+  my $sd          = $hub->species_defs;
+  my $job_data    = $job->job_data;
+  my $job_num     = $job->job_number;
+  my $species     = $job->species;
+  my $configs     = $self->_display_config($job_data->{'configs'});
+  my $two_col     = $self->new_twocol;
+  my $sequence    = $object->get_input_sequence_for_job($job);
+  my $job_summary = $self->get_job_summary($job, @_);
+  my $result_link = $job_summary->get_nodes_by_flag('view_results_link')->[0];
+
+  if ($result_link) {
+    my $download_link = $result_link->clone_node;
+    $download_link->inner_HTML('[Download results file]');
+    $download_link->set_attribute('href', $hub->url('Download', {'function' => '', 'tl' => $object->create_url_param}));
+    $result_link->parent_node->insert_after($download_link, $result_link);
+  }
+
+  $two_col->add_row('Job name',       $job_summary->render);
+  ## ParaSite: remove images
+  $two_col->add_row('Species',        $sd->tools_valid_species($species) ? $sd->species_label($species, 1) : $species =~ s/_/ /rg);
+  ## ParaSite
+  $two_col->add_row('Search type',    $object->get_param_value_caption('search_type', $job_data->{'search_type'}));
+  $two_col->add_row('Sequence',       sprintf('<div class="input-seq">&gt;%s</div>', join("\n", $sequence->{'display_id'} || '', ($sequence->{'sequence'} =~ /.{1,60}/g))));
+  $two_col->add_row('Query type',     $object->get_param_value_caption('query_type', $job_data->{'query_type'}));
+  $two_col->add_row('DB type',        $object->get_param_value_caption('db_type', $job_data->{'db_type'}));
+  $two_col->add_row('Source',         $object->get_param_value_caption('source', $job_data->{'source'}));
+  $two_col->add_row('Configurations', $configs) if $configs;
+
+  return $two_col;
+}
+
+1;
