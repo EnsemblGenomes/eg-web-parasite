4f5c97857bf64ea787a51e9db7704d7d133d5644
Bruce Bolt <bbolt@ebi.ac.uk>
2015-04-29 13:18:45 +0100

    Refactor BLAST for e80

--- modules/EnsEMBL/Web/Component/Tools/Blast/ResultsTable.pm
+++ modules/EnsEMBL/Web/Component/Tools/Blast/ResultsTable.pm
@@ -23,111 +23,89 @@ package EnsEMBL::Web::Component::Tools::Blast::ResultsTable;
 use strict;
 use warnings;
 
-use parent qw(EnsEMBL::Web::Component::Tools::Blast);
+sub table_columns {
+  ## Returns a list of columns for the results table
+  ## @param Job object
+  ## @return Arrayref of column as expected by new_table method
+  my ($self, $job) = @_;
 
-sub content {
-  my $self      = shift;
-  my $hub       = $self->hub;
-  my $object    = $self->object;
-  my $job       = $object->get_requested_job({'with_all_results' => 1});
-  my $html      = '';
+  my $glossary = EnsEMBL::Web::DBSQL::WebsiteAdaptor->new($self->hub)->fetch_glossary_lookup;
 
-  if ($job && $job->status eq 'done' && @{$job->result}) {
-
-    my $results     = $job->result;
-    my $job_data    = $job->job_data;
-    my $species     = $job->species;
-    my $source      = $job_data->{'source'};
-    my $table       = $self->new_table($source =~/latestgp/i
-      ? [
+  return [ $job->job_data->{'source'} =~/latestgp/i ? (
 ## ParaSite
-          { 'key' => 'species', 'title'=> 'Genome',           'align' => 'left',  'sort' => 'string'  },
-##
-          { 'key' => 'tid',     'title'=> 'Genomic Location', 'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'tori',    'title'=> 'Orientation',      'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'qid',     'title'=> 'Query name',       'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'qstart',  'title'=> 'Query start',      'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'qend',    'title'=> 'Query end',        'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'qori',    'title'=> 'Query ori',        'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'len',     'title'=> 'Length',           'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'score',   'title'=> 'Score',            'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'evalue',  'title'=> 'E-val',            'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'pident',  'title'=> '%ID',              'align' => 'left',  'sort' => 'numeric_hidden' },
-        ]
-      : [
+    { 'key' => 'species', 'title'=> 'Genome',               'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Genome (BLAST Results)'}                     },
+## 
+    { 'key' => 'tid',     'title'=> 'Genomic Location',     'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Genomic Location (BLAST Results)'}           },
+    { 'key' => 'gene',    'title'=> 'Overlapping Gene(s)',  'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Overlapping Genes (BLAST Results)'}          },
+    { 'key' => 'tori',    'title'=> 'Orientation',          'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Orientation (BLAST Results for genomic)'}    }
+  ) : (
 ## ParaSite
-          { 'key' => 'species', 'title'=> 'Genome',           'align' => 'left',  'sort' => 'string'  },
-##
-          { 'key' => 'tid',     'title'=> 'Subject name',     'align' => 'left',  'sort' => 'string'  },
-## EG          
-          { 'key' => 'desc',    'title'=> 'Subject description',     'align' => 'left',  'sort' => 'string'  },
-##
-          { 'key' => 'tstart',  'title'=> 'Subject start',    'align' => 'left',  'sort' => 'numeric', },
-          { 'key' => 'tend',    'title'=> 'Subject end',      'align' => 'left',  'sort' => 'numeric', },
-          { 'key' => 'tori',    'title'=> 'Subject ori',      'align' => 'left',  'sort' => 'string',  },
-          { 'key' => 'gid',     'title'=> 'Genomic Location', 'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'gori',    'title'=> 'Orientation',      'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'qid',     'title'=> 'Query name',       'align' => 'left',  'sort' => 'string'  },
-          { 'key' => 'qstart',  'title'=> 'Query start',      'align' => 'left',  'sort' => 'numeric', },
-          { 'key' => 'qend',    'title'=> 'Query end',        'align' => 'left',  'sort' => 'numeric', },
-          { 'key' => 'qori',    'title'=> 'Query ori',        'align' => 'left',  'sort' => 'string', },
-          { 'key' => 'len',     'title'=> 'Length',           'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'score',   'title'=> 'Score',            'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'evalue',  'title'=> 'E-val',            'align' => 'left',  'sort' => 'numeric' },
-          { 'key' => 'pident',  'title'=> '%ID',              'align' => 'left',  'sort' => 'numeric_hidden' },
-        ],
-      [], 
-      {
-        'data_table' => 1, 
-        'exportable' => 0, 
-        'sorting' => ['score desc'],
-## EG & ParaSite          
-        'hidden_columns' => $source =~/latestgp/i ? [3, 4, 5, 6] : [3, 4, 5, 6, 7, 8, 11] }
-##    
-    );
-
-    # Data for table rows
-    for (@$results) {
-      my $result_id     = $_->result_id;
-      my $result_data   = $_->result_data->raw;
-      my $url_param     = $object->create_url_param({'result_id' => $result_id});
-      my $location_link = $self->location_link($job, $_);
-
-      $result_data->{'options'} = {'class' => "hsp_$result_id"};
-
-      ## ParaSite: prettify the genome project name for multi-species results
-      $result_data->{'species'} = $hub->species_defs->get_config($result_data->{'species'}, 'SPECIES_COMMON_NAME'); 
-      ##
-
-      # orientation
-      $result_data->{$_} = $result_data->{$_} == 1 ? 'Forward' : 'Reverse' for grep m/ori$/, keys %$result_data;
-
-      if ($source =~ /latestgp/i) {
-        $result_data->{'tid'} = $location_link;
-      } else {
-        $result_data->{'gid'} = $location_link;
-        $result_data->{'tid'} = $self->target_link($job, $_);
-      }
-
-      # add links to query name and pid columns
-      $result_data->{'qid'} = sprintf('<span>%s</span>&nbsp;<a href="%s" class="small _ht" title="View Query Sequence">[Sequence]</a>',
-        $result_data->{'qid'},
-        $hub->url($object->get_result_url('query_sequence', $job, $_))
-      );
-      $result_data->{'pident'} = sprintf('<span>%s</span>&nbsp;<a href="%s" class="small _ht" title="View Alignment">[Alignment]</a>',
-        $result_data->{'pident'},
-        $hub->url($object->get_result_url('alignment', $job, $_))
-      );
-
-      $table->add_row($result_data);
-    }
-
-    $html .= sprintf '<h3><a rel="_blast_results_table" class="toggle _slide_toggle set_cookie open" href="#">Results table</a></h3>
-      <div class="_blast_results_table toggleable">%s</div>', $table->render;
-  }
-
-  return $html;
+    { 'key' => 'species', 'title'=> 'Genome',               'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Genome (BLAST Results)'}                     },
+## 
+    { 'key' => 'tid',     'title'=> 'Subject name',         'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Subject name (BLAST Results)'}               },
+## EG    
+    { 'key' => 'desc',    'title'=> 'Subject description',  'align' => 'left',  'sort' => 'string'  },    
+## 
+    { 'key' => 'gene',    'title'=> 'Gene hit',             'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Gene hit (BLAST Results)'}                   },
+    { 'key' => 'tstart',  'title'=> 'Subject start',        'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'Subject start (BLAST Results)'}              },
+    { 'key' => 'tend',    'title'=> 'Subject end',          'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'Subject end (BLAST Results)'}                },
+    { 'key' => 'tori',    'title'=> 'Subject ori',          'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Subject ori (BLAST Results)'}                },
+    { 'key' => 'gid',     'title'=> 'Genomic Location',     'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Genomic Location (BLAST Results)'}           },
+    { 'key' => 'gori',    'title'=> 'Orientation',          'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Orientation (BLAST Results for cDNA/protein)'}}
+  ), ( 
+    { 'key' => 'qid',     'title'=> 'Query name',           'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Query name (BLAST Results)'}, 'hidden' => 1  },  
+    { 'key' => 'qstart',  'title'=> 'Query start',          'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'Query start (BLAST Results)'}                },
+    { 'key' => 'qend',    'title'=> 'Query end',            'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'Query end (BLAST Results)'}                  },
+    { 'key' => 'qori',    'title'=> 'Query ori',            'align' => 'left',  'sort' => 'string',         'help' => $glossary->{'Query ori (BLAST Results)'},  'hidden' => 1  },
+    { 'key' => 'len',     'title'=> 'Length',               'align' => 'left',  'sort' => 'numeric_hidden', 'help' => $glossary->{'Length (BLAST Results)'}                     },
+    { 'key' => 'score',   'title'=> 'Score',                'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'Score (BLAST Results)'}                      },
+    { 'key' => 'evalue',  'title'=> 'E-val',                'align' => 'left',  'sort' => 'numeric',        'help' => $glossary->{'E-val (BLAST Results)'}                      },
+    { 'key' => 'pident',  'title'=> '%ID',                  'align' => 'left',  'sort' => 'numeric_hidden', 'help' => $glossary->{'%ID (BLAST Results)'}                        }
+  ) ];
 }
 
+sub table_options {
+  ## Returns options for rendering the results table
+  ## @param Job object
+  ## @return Hashref of table options as expected by new_table method
+  my ($self, $job) = @_;
+  return {
+    'id'          => sprintf('blast_results%s', $job->job_data->{'source'} =~ /latestgp/i ? '_1' : '_2'), # keep different session record for DataTable when saving sorting, hidden cols etc
+    'data_table'  => 1,
+    'sorting'     => ['score desc'],
+## EG  & ParaSite         
+    'hidden_columns' => $job->job_data->{'source'} =~/latestgp/i ? [3, 4, 5, 6] : [3, 4, 5, 6, 7, 8, 11] 
+##        
+  };
+}
+
+sub table_row {
+  ## Returns one row per BLAST result to be added to the results table
+  my ($self, $job, $result) = @_;
+
+  my $result_id     = $result->result_id;
+  my $result_row    = $result->result_data->raw;
+  my $url_param     = $self->object->create_url_param({'result_id' => $result_id});
+  my $urls          = $self->get_result_links($job, $result);
+
+  $result_row->{'options'} = {'class' => "hsp_$result_id"};
+  
+## ParaSite: prettify the genome project name for multi-species results
+  my $hub = $self->hub;
+  $result_row->{'species'} = $hub->species_defs->get_config($result_row->{'species'}, 'SPECIES_COMMON_NAME');
+##
+
+  # orientation columns
+  $result_row->{$_} = $result_row->{$_} == 1 ? 'Forward' : 'Reverse' for grep m/ori$/, keys %$result_row;
+
+  # columns with links
+  $result_row->{'gid'}    = $result_row->{'tid'} = qq($urls->{'location'}&nbsp;$urls->{'genomic_sequence'});
+  $result_row->{'tid'}    = $urls->{'target'} unless $job->job_data->{'source'} =~ /latestgp/i;
+  $result_row->{'gene'}   = $urls->{'gene'};
+  $result_row->{'len'}    = sprintf('<span>%s</span>&nbsp;%s', $result_row->{'len'}, $urls->{'query_sequence'});
+  $result_row->{'pident'} = sprintf('<span>%s</span>&nbsp;%s', $result_row->{'pident'}, $urls->{'alignment'});
+
+  return $result_row;
+}
 
 1;
