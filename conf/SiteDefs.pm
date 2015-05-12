0c5f1b0e8083f932a8b9057d37542dfa1343fc7c
Bruce Bolt <bbolt@ebi.ac.uk>
2015-05-12 14:10:05 +0100

    Up to WBPS3

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -21,7 +21,7 @@ use strict;
 
 sub update_conf {
 
-    $SiteDefs::SITE_RELEASE_VERSION = '2';
+    $SiteDefs::SITE_RELEASE_VERSION = '3';
     $SiteDefs::SITE_RELEASE_DATE = 'March 2015';
     $SiteDefs::ENSEMBL_USERDB_NAME = 'ensembl_accounts_pt';
     $SiteDefs::GENOMIC_UNIT = 'parasite';
