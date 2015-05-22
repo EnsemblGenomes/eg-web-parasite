aa17554c895d792534b131531fb52bd6a7b6bf29
Bruce Bolt <bbolt@ebi.ac.uk>
2015-05-22 14:46:11 +0100

    Move FTP file list

--- modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/ToolLinks.pm
@@ -29,8 +29,7 @@ sub links {
   push @links, 'specieslist',   '<a class="constant" href="/species.html">Species List</a>';
   push @links, 'blast', sprintf '<a class="constant" href="%s">BLAST</a>', $self->hub->url({'species' => '', 'type' => 'Tools', 'action' => 'Blast'}) if $sd->ENSEMBL_BLAST_ENABLED;
   push @links, 'biomart',       '<a class="constant" href="/biomart/martview/">BioMart</a>';
-  push @links, 'tools',         '<a class="constant" href="/tools.html">Tools</a>';
-  push @links, 'downloads',     '<a class="constant" href="/info/access/ftp/index.html">Downloads</a>';
+  push @links, 'downloads',     '<a class="constant" href="/ftp.html">Downloads</a>';
   push @links, 'wormbase',      '<a class="constant" href="http://www.wormbase.org">WormBase</a>';
 
   return \@links;
