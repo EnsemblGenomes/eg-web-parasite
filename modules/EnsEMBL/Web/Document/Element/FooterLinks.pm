3831ab26f42df0e79773c13c1b2d19920c70278d
Bruce Bolt <bbolt@ebi.ac.uk>
2015-06-17 14:44:02 +0100

    Added Google Analytics tracking code

--- modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -31,7 +31,20 @@ sub content {
       <a href="http://www.ebi.ac.uk/Information/termsofuse.html">EMBL-EBI Terms of use</a> | 
       <a href="http://www.ebi.ac.uk/Information/Privacy.html">Privacy</a> | 
       <a href="http://ensemblgenomes.org/info/about/cookies">Cookies</a> 
-    </div>) 
+    </div>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-24717231-5', 'auto');
+      ga('send', 'pageview');
+    
+    </script>
+
+          ) 
   ;
 }
 
