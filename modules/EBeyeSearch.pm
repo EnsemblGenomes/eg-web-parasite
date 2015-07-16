816982869d3f350575474a1d568de880b5763e29
Bruce Bolt <bbolt@ebi.ac.uk>
2015-07-16 14:55:19 +0100

    Update copyright and licence for 2015

--- modules/EBeyeSearch.pm
+++ modules/EBeyeSearch.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Copyright [2009-2015] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 =cut
+
 
 package EBeyeSearch;
 
