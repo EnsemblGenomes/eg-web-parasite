8a0fef17fa372ca27ae34aadadd29c688999703e
Bruce Bolt <bbolt@ebi.ac.uk>
2015-10-22 14:18:22 +0100

    Expand images in help documents when they are clicked

--- htdocs/components/EG_00_parasite.js
+++ htdocs/components/EG_00_parasite.js
@@ -4,3 +4,49 @@ $('.home-button').hover(function() {
   $(this).removeClass('home-button-hover');
 });
 
+$('.image-expand').click(function() {
+  var url = $(this).attr('src');
+  $('body').append(
+    $('<div></div>')
+      .attr('id', 'image-popup')
+      .css('width', '100%')
+      .css('height', '100%')
+      .css('position', 'fixed')
+      .css('top', '0px')
+      .css('bottom', '0px')
+      .css('left', '0px')
+      .css('right', '0px')
+      .css('cursor', 'pointer')
+      .click(function() {
+        $(this).remove();
+      })
+      .append(
+        $('<div></div>')
+          .css('width', '100%')
+          .css('height', '100%')
+          .css('position', 'absolute')
+          .css('background', '#000000')
+          .css('opacity', '0.4')
+          .css('zIndex', '101')
+        )
+      .append(
+        $('<div></div>')
+          .css('height', '75%')
+          .css('width', '75%')
+          .css('position', 'relative')
+          .css('text-align', 'center')
+          .css('margin', '5% auto 5% auto')
+          .css('opacity', '1')
+          .css('zIndex', '102')
+          .append(
+            $('<img></img>')
+              .attr('class', 'image-large')
+              .css('height', '100%')
+              .css('width', '100%')
+              .css('border-radius', '20px')
+              .attr('src', url)
+          )
+      )
+    ).hide().fadeIn('fast');
+});
+
