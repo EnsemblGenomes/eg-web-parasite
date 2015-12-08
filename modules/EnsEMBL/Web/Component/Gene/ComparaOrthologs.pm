b4bce54e40a3630e8034f3c1e16314caea80b5bb
Bruce Bolt <bbolt@ebi.ac.uk>
2015-12-08 11:58:10 +0000

    Offer links to both WormBase and ParaSite from the orthologues page

--- modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -185,7 +185,7 @@ sub content {
     next if $skipped{$species};
     my $domain  = $spsites->{lc($species)};
     my $display = $species_defs->species_label($species);
-    my $splink  = $domain ne $species_defs->GENOMIC_UNIT ? $hub->get_ExtURL_link($display, uc $domain, {'SPECIES'=>$species}) : "<a href=\"/$species\">$display</a>";
+    my $splink  = $domain ne $species_defs->GENOMIC_UNIT && $domain !~ /^wormbase$/i ? $hub->get_ExtURL_link($display, uc $domain, {'SPECIES'=>$species}) : "<a href=\"/$species\">$display</a>";
     
     foreach my $stable_id (sort keys %{$orthologue_list{$species}}) {
       my $orthologue = $orthologue_list{$species}{$stable_id};
@@ -207,7 +207,7 @@ sub content {
       # PARASITE
       # Check if we need to form an external link
       my $link_url; my $location_link;
-      if(grep(/$domain/, keys %$species_sets) || $domain eq $species_defs->GENOMIC_UNIT) {
+      if(grep(/$domain/, keys %$species_sets) || $domain eq $species_defs->GENOMIC_UNIT || $domain =~ /^wormbase$/i) {
       	$link_url = $hub->url({
           species => $spp,
           action  => 'Summary',
@@ -226,6 +226,8 @@ sub content {
 	      $link_url  = $hub->get_ExtURL(uc "$domain\_gene", {'SPECIES'=>$species, 'ID'=>$stable_id});
    	      $location_link = $hub->get_ExtURL(uc "$domain\_gene", {'SPECIES'=>$species, 'ID'=>$orthologue->{'location'}});
       }
+      my $wb_gene_url = $domain =~ /^wormbase$/i ? $hub->get_ExtURL_link('<br /><span class="wb-compara-out">[View gene at WormBase Central]</span>', uc "$domain\_gene", {'SPECIES'=>$species, 'ID'=>$stable_id}) : '';
+      my $wb_location_url = defined($hub->species_defs->ENSEMBL_EXTERNAL_URLS->{uc("$spp\_jbrowse")}) ? $hub->get_ExtURL_link('<br /><span class="wb-compara-out">[View region in WormBase JBrowse]</span>', uc "$spp\_jbrowse", {'SPECIES'=>$species, 'REGION'=>$orthologue->{'location'}, 'HIGHLIGHT'=>''}) : '';
       # PARASITE
 
       my $target_links = ($link_url =~ /^\// 
@@ -291,20 +293,22 @@ sub content {
         }
       }
 
-      my $id_info = qq{<p class="space-below"><a href="$link_url">$stable_id</a></p>} . join '<br />', @external;
+      my $id_info = qq{<p class="space-below"><a href="$link_url">$stable_id</a>$wb_gene_url</p>} . join '<br />', @external;
       
+## PARASITE
       my $table_details = {
         'Species'   => $splink,
         'Type'       => ucfirst $orthologue_desc,
         'dN/dS'      => $orthologue_dnds_ratio,
         'identifier' => $self->html_format ? $id_info : $stable_id,
-        'Location'   => qq{<a href="$location_link">$orthologue->{'location'}</a>},
+        'Location'   => qq{<a href="$location_link">$orthologue->{'location'}</a>$wb_location_url},
         $column_name => $self->html_format ? qq{<span class="small">$target_links</span>} : $description,
         'Target %id' => $target,
         'Query %id'  => $query,
         'options'    => { class => join(' ', 'all', @{$sets_by_species->{$species} || []}) }
       };      
       $table_details->{'Gene name(Xref)'}=$orthologue->{'display_id'} if(!$self->html_format);
+## PARASITE
       
       push @rows, $table_details;
     }
