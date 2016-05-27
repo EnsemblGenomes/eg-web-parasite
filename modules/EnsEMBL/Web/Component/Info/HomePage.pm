eeecff6b8634301d0ce9820a2bf80c16f0764c8a
Bruce Bolt <bbolt@ebi.ac.uk>
2016-05-27 14:33:37 +0100

    Use BioProject meta key

--- modules/EnsEMBL/Web/Component/Info/HomePage.pm
+++ modules/EnsEMBL/Web/Component/Info/HomePage.pm
@@ -202,8 +202,7 @@ sub content {
   $alt_string .= $alt_count == 1 ? "is $alt_count alternative genome project" : "are $alt_count alternative genome projects";
   $alt_string .= " for <em>$display_name</em> available in WormBase ParaSite: ";
   foreach my $alt (@alt_projects) {
-    my @parts = split("_", $alt);
-    my $bioproj = uc($parts[2]);
+    my $bioproj = $species_defs->get_config($alt, 'SPECIES_BIOPROJECT');
     my $provider = $species_defs->get_config($alt, 'PROVIDER_NAME');
     my $summary = $provider;
     $alt_string .= qq(<a href="/$alt/Info/Index/" title="$summary">$bioproj</a> );
