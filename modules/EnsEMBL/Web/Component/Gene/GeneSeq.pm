644c5afbf58d9a36c02bd93b5b487a54ce5fbed2
Bruce Bolt <bbolt@ebi.ac.uk>
2016-07-04 13:50:27 +0100

    Remove unnecessary parts of this module

--- modules/EnsEMBL/Web/Component/Gene/GeneSeq.pm
+++ modules/EnsEMBL/Web/Component/Gene/GeneSeq.pm
@@ -22,40 +22,6 @@ use strict;
 
 use base qw(EnsEMBL::Web::Component::TextSequence EnsEMBL::Web::Component::Gene);
 
-sub initialize {
-  my ($self, $slice, $start, $end, $adorn) = @_;
-  my $hub    = $self->hub;
-  my $object = $self->get_object;
-
-  my $config = {
-    display_width   => $hub->param('display_width') || 60,
-    site_type       => $hub->species_defs->ENSEMBL_SITETYPE,
-    gene_name       => $object->Obj->can('external_name') && $object->Obj->external_name ? $object->Obj->external_name : $object->stable_id,
-    species         => $hub->species,
-    sub_slice_start => $start,
-    sub_slice_end   => $end,
-    ambiguity       => 1,
-  };
-
-  for (qw(exon_display exon_ori snp_display line_numbering title_display)) {
-    $config->{$_} = $hub->param($_) unless $hub->param($_) eq 'off';
-  }
-
-  $config->{'exon_features'} = $object->Obj->get_all_Exons;
-  $config->{'slices'}        = [{ slice => $slice, name => $config->{'species'} }];
-  $config->{'end_number'}    = $config->{'number'} = 1 if $config->{'line_numbering'};
-
-  my ($sequence, $markup) = $self->get_sequence_data($config->{'slices'}, $config,$adorn);
-
-  $self->markup_exons($sequence, $markup, $config)     if $config->{'exon_display'};
-  if($adorn ne 'none') {
-    $self->markup_variation($sequence, $markup, $config) if $config->{'snp_display'};
-  }
-  $self->markup_line_numbers($sequence, $config)       if $config->{'line_numbering'};
-
-  return ($sequence, $config);
-}
-
 sub content {
   my $self      = shift;
   my $hub       = $self->hub;
