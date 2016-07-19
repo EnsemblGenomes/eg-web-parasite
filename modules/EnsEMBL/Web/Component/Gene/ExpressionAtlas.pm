5f78b15ee69401d724299ab42939921e029ca17a
Bruce Bolt <bbolt@ebi.ac.uk>
2016-07-19 16:51:44 +0100

    Changes to GXA widget code

--- modules/EnsEMBL/Web/Component/Gene/ExpressionAtlas.pm
+++ modules/EnsEMBL/Web/Component/Gene/ExpressionAtlas.pm
@@ -33,17 +33,18 @@ sub content {
   my $species     = $hub->species_defs->SPECIES_SCIENTIFIC_NAME;
 ##
   my $html;
-  
+
   $species        =~ s/_/ /gi; #GXA require the species with no underscore.  
   if (!$hub->gxa_status) {
     $html = $self->_info_panel("error", "Gene expression atlas site down!", "<p>The widget cannot be displayed as the gene expression atlas site is down. Please check again later.</p>");
   } else {
     #this script tag has been kept here as it was easier to call the perl param within the script tag (the js file wasn't getting the param)
 ## ParaSite
+    (my $gxa_url = $SiteDefs::GXA_EBI_URL) =~ s/\/gxa\/$//;
     $html = sprintf(qq{
       <script type="text/javascript">
         expressionAtlasHeatmapHighcharts.render ({
-              gxaBaseUrl: "%s",
+              atlasHost: "%s",
               params:'geneQuery=%s&species=%s&source=DEVELOPMENTAL_STAGE',
               isMultiExperiment: true,
               showAnatomogram: false,
@@ -52,10 +53,9 @@ sub content {
         });
       </script>  
       <div id="expressionAtlas"></div>    
-    }, $SiteDefs::GXA_EBI_URL, $stable_id, $species);
+    }, $gxa_url, $stable_id, $species);
 ##
   }
-
   return $html;
 }
 
