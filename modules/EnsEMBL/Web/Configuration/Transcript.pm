a19679859881892139dee60206c63296369979ae
Bruce Bolt <bbolt@ebi.ac.uk>
2016-07-20 15:53:29 +0100

    Only show the variation menu options if EVA track is available

--- modules/EnsEMBL/Web/Configuration/Transcript.pm
+++ modules/EnsEMBL/Web/Configuration/Transcript.pm
@@ -39,15 +39,17 @@ sub modify_tree {
   $self->delete_node('UserAnnotation');
   $self->delete_node('History');
   
-  my $variation = $self->create_node('EVA', 'Variation', [],
-    { availability => 0 }
-  );
-  $variation->append(
-    $self->create_node('EVA_Table', 'Variation Table',
-      [qw(eva_table EnsEMBL::Web::Component::Transcript::EVA_Table)]
-    )
-  );
-
+  if($self->hub->species_defs->EVA_TRACKS) {
+    my $variation = $self->create_node('EVA', 'Variation', [],
+      { availability => 0 }
+    );
+    $variation->append(
+      $self->create_node('EVA_Table', 'Variation Table',
+        [qw(eva_table EnsEMBL::Web::Component::Transcript::EVA_Table)]
+      )
+    );
+  }
+  
   my $summary = $self->get_node('Summary');
   $summary->set('components',
     [qw(
