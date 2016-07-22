bf99805334456443dfcc376b2feb4946c6d94954
Bruce Bolt <bbolt@ebi.ac.uk>
2016-07-22 11:22:21 +0100

    Modify Ensembl piechart code to not assume the ordering of data+charts

---
+++ htdocs/components/EG_10_Piechart.js
@@ -0,0 +1,36 @@
+Ensembl.Panel.Piechart.prototype.getContent = function() {
+  var panel   = this;
+  var visible = [];
+
+  this.graphData   = [];
+  this.graphConfig = {};
+  this.graphEls    = {};
+  this.dimensions  = eval($('input.graph_dimensions', this.el).val());
+
+  // ParaSite: sometimes the order of items in the DOM doesn't match the assigned item number
+  if($('input.graph_data_ordered').length > 0) {
+    $('input.graph_data_ordered', this.el).each(function () {
+      var id = $(this).attr('id').replace('graph_data_item_', '');
+      panel.graphData[id] = eval(this.value);
+    });
+  } else {
+    $('input.graph_data', this.el).each(function () {
+      panel.graphData.push(eval(this.value));
+    });
+  }
+  //
+
+  $('input.graph_config', this.el).each(function () {
+    panel.graphConfig[this.name] = eval(this.value);
+  });
+
+  for (i in this.graphData) {
+    this.graphEls[i] = $('#graphHolder' + i);
+
+    if (this.graphEls[i].is(':visible')) {
+      visible.push(i);
+    }
+  }
+
+  this.makeGraphs(visible);
+};
