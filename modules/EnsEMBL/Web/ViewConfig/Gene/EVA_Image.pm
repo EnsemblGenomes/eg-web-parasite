3712ff2a4f3eae3600f36588bbce80d95c42c9cd
Bruce Bolt <bbolt@ebi.ac.uk>
2016-10-06 16:09:42 +0100

    Fix the ViewConfig for EVA variant image

--- modules/EnsEMBL/Web/ViewConfig/Gene/EVA_Image.pm
+++ modules/EnsEMBL/Web/ViewConfig/Gene/EVA_Image.pm
@@ -10,4 +10,14 @@ sub init {
   $self->set_default_options({ 'context' => 100 });
 }
 
+sub field_order { } # no default fields
+sub form_fields { } # no default fields
+
+sub init_form {
+  my $self  = shift;
+  my $form  = $self->SUPER::init_form(@_);
+
+  return $form;
+}
+
 1;
