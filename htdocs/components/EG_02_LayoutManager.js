559e16362d0a35c892c7e97d3563cbd9476e9ded
Bruce Bolt <bbolt@ebi.ac.uk>
2016-10-18 14:39:03 +0100

    Update style on cookie warning

--- htdocs/components/EG_02_LayoutManager.js
+++ htdocs/components/EG_02_LayoutManager.js
@@ -3,16 +3,16 @@ Ensembl.LayoutManager.showCookieMessage = function() {
 
   if (!cookiesAccepted) {
     $(['<div class="cookie-message hidden">',
-      '<div></div>',
-      '<p>We use cookies to enhance the usability of our website. If you continue, we\'ll assume that you are happy to receive all cookies.</p>',
-      '<p><button>Don\'t show this again</button></p>',
-      '<p>Read more about EMBL-EBI policies: <a href="http://www.ebi.ac.uk/about/cookie-control">Cookies</a> | <a href="http://www.ebi.ac.uk/about/privacy">Privacy</a>.</p>',
+      '<p class="msg">We use cookies to enhance the usability of our website. If you continue, we\'ll assume that you are happy to receive all cookies.&nbsp;',
+      '<span class="more-info">Read more about EMBL-EBI policies: <a href="http://www.ebi.ac.uk/about/cookie-control">Cookies</a> | <a href="http://www.ebi.ac.uk/about/privacy">Privacy</a>.</span>',
+      '</p>',
+      '<span class="close">x</span>',
       '</div>'
     ].join(''))
-      .appendTo(document.body).show().find('button,div').on('click', function (e) {
+      .appendTo(document.body).show().find('span.close').on('click', function (e) {
         Ensembl.cookie.set('cookies_ok', 'yes');
         $(this).parents('div').first().fadeOut(200);
-    }).filter('div').helptip({content:"Don't show this again"});
+    });
     return true;
   }
 
