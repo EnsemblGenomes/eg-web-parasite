e12981a00a249a2d02bb2e1624555fa4dd497efc
Bruce Bolt <bbolt@ebi.ac.uk>
2016-11-22 12:13:00 +0000

    Change regex to get species name only

--- modules/EnsEMBL/Web/Parsers/Blast.pm
+++ modules/EnsEMBL/Web/Parsers/Blast.pm
@@ -37,7 +37,7 @@ sub parse_xml {
     my ($description) = $hit->{description} =~ /description:"(.+)"/;
 
 ## ParaSite: determine the species for each hit (rather than each job) as it is possible to have a single job query multiple species
-    my ($species) = $hit->{description} =~ /species:(.+)/;  # This works becuase we have species:xxxxxx appended to the end of the FASTA header line in the dumps
+    my ($species) = $hit->{description} =~ /species:(\S*)/;
     my $db = $hub->database('core', $species);
     $hit_id =~ s/$species://;
 ## ParaSite
