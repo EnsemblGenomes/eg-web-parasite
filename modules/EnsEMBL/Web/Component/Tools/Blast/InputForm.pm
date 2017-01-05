b52f919efc2604d4230e27583849e94da7a72fb9
Bruce Bolt <bbolt@ebi.ac.uk>
2017-01-05 09:22:55 +0000

    Update copyright for 2017

--- modules/EnsEMBL/Web/Component/Tools/Blast/InputForm.pm
+++ modules/EnsEMBL/Web/Component/Tools/Blast/InputForm.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2014-2016] EMBL-European Bioinformatics Institute
+Copyright [2014-2017] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,7 +10,8 @@ You may obtain a copy of the License at
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.See the License for the specific language governing permissions and
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
 limitations under the License.
 
 =cut
