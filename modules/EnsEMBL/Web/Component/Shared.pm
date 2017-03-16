9993d7d33a700487b96d7d15ad41145e492a43f4
Bruce Bolt <bbolt@ebi.ac.uk>
2017-03-16 13:38:23 +0000

    Remove space from region

--- modules/EnsEMBL/Web/Component/Shared.pm
+++ modules/EnsEMBL/Web/Component/Shared.pm
@@ -93,7 +93,7 @@ sub transcript_table {
   my $seq_region_end   = $object->seq_region_end;
 
   my $location_html = sprintf(
-    '<a href="%s" class="constant mobile-nolink">%s: %s-%s</a> %s.',
+    '<a href="%s" class="constant mobile-nolink">%s:%s-%s</a> %s.',
     $hub->url({
       type   => 'Location',
       action => 'View',
