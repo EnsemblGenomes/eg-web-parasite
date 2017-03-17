0f1343eab1e384fc53dfd25a0d14bbb7da0582fb
Bruce Bolt <bbolt@ebi.ac.uk>
2017-03-17 14:48:38 +0000

    Change order of legend in BUSCO circle

--- htdocs/components/EG_02_circle_plot.js
+++ htdocs/components/EG_02_circle_plot.js
@@ -468,10 +468,13 @@ Assembly.prototype.drawPlot = function(parent_div, longest, circle_span) {
       .attr('class', 'asm-tr_title');
     txt.append('tspan').text('BUSCO (n = ' + this.busco.n.toLocaleString() + ')');
     var key = lccg.append('g').attr('transform', 'translate(' + (size / 2 + 60) + ',' + (-size / 2 + 345) + ')');
-    key.append('rect').attr('height', w).attr('width', w).attr('class', 'asm-busco_D asm-toggle');
-    key.append('text').attr('x', w + 3).attr('y', w - 1).text('Duplicated (' + this.busco.D.toFixed(1) + '%)').attr('class', 'asm-key');
-    key.append('rect').attr('y', w * 1.5).attr('height', w).attr('width', w).attr('class', 'asm-busco_C asm-toggle');
-    key.append('text').attr('x', w + 3).attr('y', w * 2.5 - 1).text('Single (' + (this.busco.C - this.busco.D).toFixed(1) + '%)').attr('class', 'asm-key');
+
+    key.append('rect').attr('height', w).attr('width', w).attr('class', 'asm-busco_C asm-toggle');
+    key.append('text').attr('x', w + 3).attr('y', w - 1).text('Single (' + (this.busco.C - this.busco.D).toFixed(1) + '%)').attr('class', 'asm-key');
+
+    key.append('rect').attr('y', w * 1.5).attr('height', w).attr('width', w).attr('class', 'asm-busco_D asm-toggle');
+    key.append('text').attr('x', w + 3).attr('y', w * 2.5 - 1).text('Duplicated (' + this.busco.D.toFixed(1) + '%)').attr('class', 'asm-key');
+
     key.append('rect').attr('y', w * 3).attr('height', w).attr('width', w).attr('class', 'asm-busco_F asm-toggle');
     key.append('text').attr('x', w + 3).attr('y', w * 4 - 1).text('Fragmented (' + this.busco.F.toFixed(1) + '%)').attr('class', 'asm-key');
   }
