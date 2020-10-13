b176fd28d1d93529961255a8ae75ff532be96a1e
Tuan Le <tuanle@ebi.ac.uk>
2020-10-13 13:56:10 +0000

    Make the external reference column link to the publication

--- modules/EnsEMBL/Web/Component/Phenotype/Locations.pm
+++ modules/EnsEMBL/Web/Component/Phenotype/Locations.pm
@@ -51,6 +51,137 @@ sub content {
   return $html;
 }
 
+sub table_content {
+  my ($self,$callback) = @_;
+
+  my $hub = $self->hub;
+
+  my $ph_id              = $hub->param('ph');
+  my $ontology_accession = $hub->param('oa');
+ 
+  my $gene_ad = $hub->database('core')->get_adaptor('Gene');
+  my $pf_ad   = $hub->database('variation')->get_adaptor('PhenotypeFeature');
+
+  my (%gene_ids,$pfs);
+
+  if ($ph_id) {
+    $pfs = $pf_ad->fetch_all_by_phenotype_id_source_name($ph_id);
+  }
+  else {
+    $pfs = $pf_ad->fetch_all_by_phenotype_accession_source($ontology_accession);
+  }
+
+  ROWS: foreach my $pf (@{$pfs}) {
+    next if $callback->free_wheel();
+
+    unless($callback->phase eq 'outline') {
+      
+      my $feat_type = $pf->type;
+        
+      next if ($feat_type eq 'SupportingStructuralVariation');
+
+      if ($feat_type eq 'Variation') {
+        $feat_type = 'Variant';
+      } elsif ($feat_type eq 'StructuralVariation') {
+        $feat_type = 'Structural Variant';
+      }
+
+      my $pf_name      = $pf->object_id;
+      my $region       = $pf->seq_region_name;
+      my $start        = $pf->seq_region_start;
+      my $end          = $pf->seq_region_end;
+      my $strand       = $pf->seq_region_strand;
+      my $strand_label = ($strand == 1) ? '+' : '-';
+      my $phe_desc     = $pf->phenotype_description; 
+      my $study_xref   = ($pf->study) ? $pf->study->external_reference : undef;
+      my $study_title  = ($pf->study) ? $pf->study->name : undef;
+      my $external_id  = ($pf->external_id) ? $pf->external_id : undef;
+      my $attribs      = $pf->get_all_attributes;
+      my $source       = $pf->source_name;  
+      my ($source_text,$source_url) = $self->source_url($pf_name, $source, $external_id, $attribs, $pf);
+
+      my @reported_genes = split(/,/,$pf->associated_gene);
+
+      my @assoc_genes;
+      # preparing the URL for all the associated genes and ignoring duplicate one
+      foreach my $id (@reported_genes) {
+        $id =~ s/\s//g;
+        next if $id =~ /intergenic|pseudogene/i || $id eq 'NR';
+      
+        my $gene_label = [$id,undef,undef];
+
+        if (!$gene_ids{$id}) {
+          foreach my $gene (@{$gene_ad->fetch_all_by_external_name($id) || []}) {
+            $gene_ids{$id} = $gene->description;
+          }
+        }
+
+        if ($gene_ids{$id}) {
+          $gene_label = [$id,$hub->url({ type => 'Gene', action => 'Summary', g => $id }),$gene_ids{$id}];
+        }
+        push @assoc_genes,$gene_label;
+      }
+
+      my $studies = $self->study_urls($study_xref, $study_title);
+      my ($name_id,$name_url,$name_extra) = $self->pf_link($pf,$pf->type,$pf->phenotype_id);
+
+      # ClinVar specific data
+      my $evidence_list;
+      my $submitter_list = [];
+      if ($source =~ /clinvar/i) {
+        if ($attribs->{'MIM'}) {
+          my @data = split(',',$attribs->{'MIM'});
+          foreach my $ext_ref (@data) {
+            push(@$studies, [$hub->get_ExtURL('OMIM', $ext_ref),'MIM:'.$ext_ref]);
+          }
+        }
+        if ($attribs->{'pubmed_id'}) {
+          my @data = split(',',$attribs->{'pubmed_id'});
+          $evidence_list = $self->supporting_evidence_link(\@data, 'pubmed_id');
+        }
+        # Submitter data
+        $submitter_list = $pf->submitter_names;
+      }
+ 
+      my @study_links    = map { $_->[0]||'' } @$studies;
+      my @study_texts    = map { $_->[1]||'' } @$studies;
+      my @evidence_texts = ($evidence_list) ? keys(%$evidence_list) : ();
+      my @evidence_links = ($evidence_list) ? map { $evidence_list->{$_} } @evidence_texts : ();
+      my @gene_texts     = map { $_->[0]||'' } @assoc_genes;
+      my @gene_links     = map { $_->[1]||'' } @assoc_genes;
+      my @gene_titles    = map { $_->[2]||'' } @assoc_genes;
+
+
+      my $row = {
+           name_id          => $name_id,
+           name_link        => $name_url,
+           name_extra       => $name_extra,
+           location         => "$region:$start-$end$strand_label",
+           feature_type     => $feat_type,
+           phe_source       => $source_text,
+           phe_link         => $source_url,
+           study_links      => join("\r",'',@study_links),
+           study_texts      => join("\r",'',@study_texts),
+           study_submitter  => join(', ', $submitter_list ? @$submitter_list : ()),
+           evidence_links   => join("\r",'',@evidence_links),
+           evidence_texts   => join("\r",'',@evidence_texts),
+           gene_links       => join("\r",'',@gene_links),
+           gene_texts       => join("\r",'',@gene_texts),
+           gene_titles      => join("\r",'',@gene_titles),
+      };
+
+      if (!$hub->param('ph')) {
+        $row->{phe_desc} = $self->phenotype_url($phe_desc,$pf->phenotype_id);
+        $row->{p_desc}   = $phe_desc;
+      }
+
+      $callback->add_row($row);
+      last ROWS if $callback->stand_down;
+    }
+  }
+}
+
+
 ## ParaSite: remove some unpopulated columns 
 sub make_table {
   my $self = shift;
