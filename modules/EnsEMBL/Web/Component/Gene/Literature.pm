170df9ef07c9e9b1b88c56a99e57c7f8e1491742
Tuan Le <tuanle@ebi.ac.uk>
2021-09-03 14:00:46 +0000

    Add synonyms to Europe PMC literature search

---
+++ modules/EnsEMBL/Web/Component/Gene/Literature.pm
@@ -0,0 +1,100 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Component::Gene::Literature;
+use strict;
+use base qw(EnsEMBL::Web::Component::Gene);
+use Data::Dumper;
+use URI::Escape;
+use JSON;
+use LWP::UserAgent;
+
+# Change of query for PARASITE-439
+
+sub content {
+  my $self         = shift;
+  my $hub          = $self->hub;
+  my $object       = $self->object;
+  my $html;
+  
+  my $query = sprintf '("%s") AND ("%s")', join('" OR "', @{$self->get_gene_names}),$hub->species_defs->SPECIES_SCIENTIFIC_NAME;
+  my ($articles, $error) = $self->europe_pmc_articles($query.'&pageSize=50');
+
+  if ($error) {
+  
+    $html .= $self->_info_panel('error', 'Failed to fetch articles from EuropePMC', $error);
+  
+  } else {
+
+    my $table = $self->new_table(
+      [
+        { key => 'pubmed_id', title => 'PubMed&nbsp;ID', width => '6%',  align => 'left', sort => 'html' },
+        { key => 'title',     title => 'Title',          width => '50%', align => 'left', sort => 'string' },
+        { key => 'authors',   title => 'Authors',        width => '22%', align => 'left', sort => 'html' },
+        { key => 'journal',   title => 'Journal',        width => '22%', align => 'left', sort => 'string' },
+      ], 
+      [], 
+      { 
+        class      => 'no_col_toggle',
+        data_table => 1, 
+        exportable => 0,
+        data_table_config => {
+            iDisplayLength => 10
+        },
+      }
+    );
+
+    foreach (@$articles) {
+      my @authors = split /\s*,\s+|\s*and\s+/, $_->{authorString};
+      @authors = map {sprintf '<a href="http://europepmc.org/search?page=1&query=%s">%s</a>', uri_escape(qq(AUTH:"$_")), $_  } @authors;
+
+      $table->add_row({
+        pubmed_id => sprintf( '<a href="%s" style="white-space:nowrap">%s</a>', $hub->get_ExtURL('PUBMED', $_->{pmid}), $_->{pmid} ),
+        title     => $_->{title},
+        authors   => join(', ', @authors),
+        journal   => sprintf '%s %s(%s) %s', $_->{journalTitle}, $_->{journalVolume}, $_->{issue}, $_->{pubYear}
+      });
+    }
+
+    $html .= scalar(@$articles) == 0 ?
+             sprintf '<p>There are no hits for this gene from <a href="https://europepmc.org/search?query=%s">Europe PubMed Central</a></p>', uri_escape($query) : 
+             sprintf '<p>Showing the top %d hits from <a href="https://europepmc.org/search?query=%s">Europe PubMed Central</a></p>', scalar(@$articles), uri_escape($query);
+    $html .= $table->render;  
+  }
+
+  return $html;
+}
+
+sub get_gene_names {
+  my $self   = shift;
+  my $obj    = $self->object->Obj;
+  my @names  = ($obj->display_id);
+
+  if ($obj->can('display_xref')) {
+    if (my $xref = $obj->display_xref) {
+      push @names, $xref->display_id;
+      push @names, @{$xref->get_all_synonyms};
+    }
+  }
+  
+  return \@names;
+}
+
+
+1;
+
