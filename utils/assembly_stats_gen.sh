25216549a5843b8d1e61980b7248adf5ff06a81c
Tuan Le <tuanle@ebi.ac.uk>
2023-03-24 17:30:33 +0000

    Improve assembly stats script

--- utils/assembly_stats_gen.sh
+++ utils/assembly_stats_gen.sh
@@ -4,6 +4,7 @@ DB_SERVER=mysql-ps-staging-2
 DB_PORT=4467
 PS_VERSION=16
 ENS_VERSION=101
+OUTPUT_PATH=/nfs/public/rw/ensweb/parasite/assembly_stats
 
 function generate_stat() {
 	DB=$1
@@ -11,8 +12,11 @@ function generate_stat() {
 	species_json="assembly_$species.json"
 	echo "New json file: $species_json"
 
-	bsub -o /dev/null -e /dev/null perl -I ensembl/modules/ eg-web-parasite/utils/assembly_stats.pl --host $DB_SERVER --port $DB_PORT --user ensro --busco-an --busco-as --assembly --dbname $DB --outfile /nfs/services/nobackup/ensweb/parasite/assembly_stats/$species_json
+	bsub -o /dev/null -e /dev/null perl -I ensembl/modules/ eg-web-parasite/utils/assembly_stats.pl --host $DB_SERVER --port $DB_PORT --user ensro --busco-an --busco-as --assembly --dbname $DB --outfile $OUTPUT_PATH/$species_json
 }
+
+echo "Cleaning the output dir: $OUTPUT_PATH"
+rm $OUTPUT_PATH/*
 
 mysql -u ensro -h $DB_SERVER -NB -P $DB_PORT -e 'SHOW DATABASES LIKE "%core_'$PS_VERSION'_'$ENS_VERSION'%"' | while read db_core
 do
